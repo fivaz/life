880eaa76e039a72fce97922601d01bc3785b15f6
fivaz <s.a.fivaz@gmail.com>
2023-10-12 02:14:50 +0200

    wip calendar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,105 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { differenceInMinutes, format, getDate, parse } from 'date-fns';
+	import classnames from "classnames";
 
 	let container: HTMLDivElement;
 	let containerNav: HTMLDivElement;
 	let containerOffset: HTMLDivElement;
 
+	let events = [
+		{
+			label: 'Breakfast',
+			startTime: '2023-10-12T06:00',
+			endTime: '2023-10-12T07:00'
+		},
+		{
+			label: 'Flight to Paris',
+			startTime: '2023-10-12T07:30',
+			endTime: '2023-10-12T10:00'
+		},
+		{
+			label: 'Meeting with design team at Disney',
+			startTime: '2023-10-15T10:00',
+			endTime: '2023-10-15T12:00'
+		}
+	];
+
+	// Randomize colors for each event
+	function getRandomColorClass() {
+		const colors = [
+			'bg-blue-50',
+			'bg-pink-50',
+			'bg-yellow-50',
+			'bg-green-50',
+			'bg-purple-50',
+			'bg-red-50',
+			'bg-indigo-50'
+		];
+		return colors[Math.floor(Math.random() * colors.length)];
+	}
+
+	function calculateGridRowSpan(startTime: string, endTime: string): number {
+		const start = parse(startTime, "yyyy-MM-dd'T'HH:mm", new Date());
+		const end = parse(endTime, "yyyy-MM-dd'T'HH:mm", new Date());
+		const minutesDifference = differenceInMinutes(end, start);
+		return Math.ceil(minutesDifference / 15); // Each quarter-hour corresponds to 1 grid row
+	}
+
+	function calculateGridRowPosition(startTime: string): number {
+		const start = parse(startTime, "yyyy-MM-dd'T'HH:mm", new Date());
+		const hours = start.getHours();
+		return 74 + hours * 4;
+	}
+
+	function calculateGridColumnPosition(startTime: string): number {
+		const start = parse(startTime, "yyyy-MM-dd'T'HH:mm", new Date());
+		const dayOfMonth = getDate(start);
+		return dayOfMonth - 10; // Adjust the column based on the day of the month
+	}
+
+	let daysAndDates = [
+		{ day: 'Mon', dayShort: 'M', date: 10 },
+		{ day: 'Tue', dayShort: 'T', date: 11 },
+		{ day: 'Wed', dayShort: 'W', date: 12 },
+		{ day: 'Thu', dayShort: 'T', date: 13 },
+		{ day: 'Fri', dayShort: 'F', date: 14 },
+		{ day: 'Sat', dayShort: 'S', date: 15 },
+		{ day: 'Sun', dayShort: 'S', date: 16 }
+	];
+
+	let timeLabels = [
+		'12AM',
+		'1AM',
+		'2AM',
+		'3AM',
+		'4AM',
+		'5AM',
+		'6AM',
+		'7AM',
+		'8AM',
+		'9AM',
+		'10AM',
+		'11AM',
+		'12PM',
+		'1PM',
+		'2PM',
+		'3PM',
+		'4PM',
+		'5PM',
+		'6PM',
+		'7PM',
+		'8PM',
+		'9PM',
+		'10PM',
+		'11PM'
+	];
+
+	const currentDate = new Date();
+
 	onMount(() => {
 		// Set the container scroll position based on the current time.
-		const currentMinute = new Date().getHours() * 60;
+		const currentMinute = currentDate.getHours() * 60;
 
 		console.log(currentMinute);
 
@@ -75,99 +167,45 @@
 				class="sticky top-0 z-30 flex-none bg-white shadow ring-1 ring-black ring-opacity-5 sm:pr-8"
 			>
 				<div class="grid grid-cols-7 text-sm leading-6 text-gray-500 sm:hidden">
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						M
-						<span class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
-							>10</span
-						>
-					</button>
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						T
-						<span class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
-							>11</span
-						>
-					</button>
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						W
-						<span
-							class="mt-1 flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white"
-							>12</span
-						>
-					</button>
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						T
-						<span class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
-							>13</span
-						>
-					</button>
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						F
-						<span class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
-							>14</span
-						>
-					</button>
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						S
-						<span class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
-							>15</span
-						>
-					</button>
-					<button type="button" class="flex flex-col items-center pt-2 pb-3">
-						S
-						<span class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
-							>16</span
-						>
-					</button>
+					{#each daysAndDates as { day, date } (day)}
+						<button type="button" class="flex flex-col items-center pt-2 pb-3">
+							{day}
+							{#if getDate(currentDate) === date}
+								<span
+									class="mt-1 flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white"
+								>
+									{date}
+								</span>
+							{:else}
+								<span
+									class="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900"
+									>{date}
+								</span>
+							{/if}
+						</button>
+					{/each}
 				</div>
 
 				<div
 					class="-mr-px hidden grid-cols-7 divide-x divide-gray-100 border-r border-gray-100 text-sm leading-6 text-gray-500 sm:grid"
 				>
 					<div class="col-end-1 w-14" />
-					<div class="flex items-center justify-center py-3">
-						<span
-							>Mon <span class="items-center justify-center font-semibold text-gray-900">10</span
-							></span
-						>
-					</div>
-					<div class="flex items-center justify-center py-3">
-						<span
-							>Tue <span class="items-center justify-center font-semibold text-gray-900">11</span
-							></span
-						>
-					</div>
-					<div class="flex items-center justify-center py-3">
-						<span class="flex items-baseline"
-							>Wed <span
-								class="ml-1.5 flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white"
-								>12</span
-							></span
-						>
-					</div>
-					<div class="flex items-center justify-center py-3">
-						<span
-							>Thu <span class="items-center justify-center font-semibold text-gray-900">13</span
-							></span
-						>
-					</div>
-					<div class="flex items-center justify-center py-3">
-						<span
-							>Fri <span class="items-center justify-center font-semibold text-gray-900">14</span
-							></span
-						>
-					</div>
-					<div class="flex items-center justify-center py-3">
-						<span
-							>Sat <span class="items-center justify-center font-semibold text-gray-900">15</span
-							></span
-						>
-					</div>
-					<div class="flex items-center justify-center py-3">
-						<span
-							>Sun <span class="items-center justify-center font-semibold text-gray-900">16</span
-							></span
-						>
-					</div>
+					{#each daysAndDates as { day, date } (day)}
+						<div class="flex items-center justify-center py-3">
+							<span>
+								{day}
+								{#if getDate(currentDate) === date}
+									<span
+										class="items-center justify-center rounded-full bg-indigo-600 font-semibold text-white ml-1.5 flex h-8 w-8"
+										>{date}
+									</span>
+								{:else}
+									<span class="items-center justify-center font-semibold text-gray-900">{date}</span
+									>
+								{/if}
+							</span>
+						</div>
+					{/each}
 				</div>
 			</div>
 			<div class="flex flex-auto">
@@ -179,198 +217,16 @@
 						style="grid-template-rows: repeat(48, minmax(3.5rem, 1fr))"
 					>
 						<div bind:this={containerOffset} class="row-end-1 h-7" />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								12AM
+						{#each timeLabels as timeLabel (timeLabel)}
+							<div>
+								<div
+									class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
+								>
+									{timeLabel}
+								</div>
 							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								1AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								2AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								3AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								4AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								5AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								6AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								7AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								8AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								9AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								10AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								11AM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								12PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								1PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								2PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								3PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								4PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								5PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								6PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								7PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								8PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								9PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								10PM
-							</div>
-						</div>
-						<div />
-						<div>
-							<div
-								class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-gray-400"
-							>
-								11PM
-							</div>
-						</div>
-						<div />
+							<div />
+						{/each}
 					</div>
 
 					<!-- Vertical lines -->
@@ -387,47 +243,26 @@
 						<div class="col-start-8 row-span-full w-8" />
 					</div>
 
-					<!-- Events -->
-					<ol
-						class="col-start-1 col-end-2 row-start-1 grid grid-cols-1 sm:grid-cols-7 sm:pr-8"
-						style="grid-template-rows: 1.75rem repeat(288, minmax(0, 1fr)) auto"
-					>
-						<li class="relative mt-px flex sm:col-start-3" style="grid-row: 74 / span 12">
-							<div
-								class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-blue-100"
-							>
-								<p class="order-1 font-semibold text-blue-700">Breakfast</p>
-								<p class="text-blue-500 group-hover:text-blue-700">
-									<time datetime="2022-01-12T06:00">6:00 AM</time>
-								</p>
-							</div>
-						</li>
-						<li class="relative mt-px flex sm:col-start-3" style="grid-row: 92 / span 30">
-							<div
-								class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-pink-50 p-2 text-xs leading-5 hover:bg-pink-100"
-							>
-								<p class="order-1 font-semibold text-pink-700">Flight to Paris</p>
-								<p class="text-pink-500 group-hover:text-pink-700">
-									<time datetime="2022-01-12T07:30">7:30 AM</time>
-								</p>
-							</div>
-						</li>
-						<li
-							class="relative mt-px hidden sm:col-start-6 sm:flex"
-							style="grid-row: 122 / span 24"
-						>
-							<div
-								class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-gray-100 p-2 text-xs leading-5 hover:bg-gray-200"
-							>
-								<p class="order-1 font-semibold text-gray-700">
-									Meeting with design team at Disney
-								</p>
+					Eventse
+					<ol class="col-start-1 col-end-2 row-start-1 grid grid-cols-7 sm:grid-cols-7 sm:pr-8" style="grid-template-rows: 1.75rem repeat(288, minmax(0, 1fr)) auto">
+						{#each events as { label, startTime, endTime } (label)}
+							{#if label === "Breakfast"}
+								<li class={classnames("relative mt-px flex sm:col-start-", 4 + calculateGridColumnPosition(startTime))} style="grid-row: {calculateGridRowPosition(startTime)} / span {calculateGridRowSpan(startTime, endTime)}">
+							{:else if label === "Flight to Paris"}
+								<li class={classnames("relative mt-px flex sm:col-start-", 4 + calculateGridColumnPosition(startTime))} style="grid-row: {calculateGridRowPosition(startTime)} / span {calculateGridRowSpan(startTime, endTime)}">
+							{:else if label === "Meeting with design team at Disney"}
+								<li class={classnames("relative mt-px flex sm:col-start-", 4 + calculateGridColumnPosition(startTime))} style="grid-row: {calculateGridRowPosition(startTime)} / span {calculateGridRowSpan(startTime, endTime)}">
+							{/if}
+							<div class={classnames("group absolute inset-1 flex flex-col overflow-y-auto rounded-lg", getRandomColorClass(), "p-2 text-xs leading-5 hover:bg-blue-100")}>
+								<p class="order-1 font-semibold text-gray-700">{label}</p>
 								<p class="text-gray-500 group-hover:text-gray-700">
-									<time datetime="2022-01-15T10:00">10:00 AM</time>
+									<time datetime={startTime}>{format(parse(startTime, "yyyy-MM-dd'T'HH:mm", new Date()), "h:mm a")}</time>
 								</p>
 							</div>
-						</li>
+							</li>
+						{/each}
 					</ol>
+
 				</div>
 			</div>
 		</div>
