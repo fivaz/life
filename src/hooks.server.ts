c49901a26073cf847c26d5e46f76fe8dab7f0d6d
fivaz <s.a.fivaz@gmail.com>
2023-10-26 09:50:31 +0200

    revert changes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,32 +2,35 @@ import GitHub from '@auth/core/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import type { Handle } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
 import prisma from '$lib/prisma';
+import { dashboardRoute, loginRoute } from '$lib/utils';
 
 const authorization: Handle = async ({ event, resolve }) => {
-	// if (event.url.pathname.startsWith(dashboardRoute)) {
-	// 	const session = await event.locals.getSession();
-	// if (!session) {
-	// 	throw redirect(303, loginRoute);
-	// }
-	// }
-
+	if (event.url.pathname.startsWith(dashboardRoute)) {
+		const session = await event.locals.getSession();
+		if (!session) {
+			throw redirect(303, loginRoute);
+		}
+	}
 	// If the request is still here, just proceed as normally
 	return resolve(event);
 };
 
-export const handle: Handle = SvelteKitAuth({
-	adapter: PrismaAdapter(prisma),
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-	callbacks: {
-		async session({ session, user, token }) {
-			console.log('session', session);
-			console.log('user', user);
-			if (user) {
-				session.user.id = user.id;
-			}
-			return session;
+export const handle: Handle = sequence(
+	SvelteKitAuth({
+		adapter: PrismaAdapter(prisma),
+		providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+		callbacks: {
+			async session({ session, user, token }) {
+				if (user) {
+					session.user.id = user.id;
+				}
+				return session;
+			},
 		},
-	},
-});
+	}),
+	authorization,
+);
