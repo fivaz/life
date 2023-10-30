6be3df6ecf53bec7c11deb702541c1fe1efc0bc3
fivaz <s.a.fivaz@gmail.com>
2023-10-30 22:32:15 +0100

    add notification component

---
+++ src/lib/components/notification/notification.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Notification from './Notification.svelte';
+
+	export const meta = {
+		component: Notification,
+		tags: ['autodocs'],
+		parameters: {
+			layout: 'fullscreen',
+		},
+	} satisfies Meta<Notification>;
+</script>
+
+<Template let:args>
+	<div class="w-96 h-96">
+		<Notification {...args} />
+	</div>
+</Template>
+
+<Story
+	name="Primary"
+	args={{
+		show: true,
+	}}
+/>
