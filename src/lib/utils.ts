141043acd7b3dafc0f4aac3fe2b0619f78b92929
fivaz <s.a.fivaz@gmail.com>
2023-11-02 16:41:45 +0100

    add dates to ToDoRow

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 export const DATE = 'yyyy-MM-dd';
 export const TIME = 'HH:mm';
 
+export const DATETIME = 'dd.MM.yyyy HH:mm';
+
 export const loginRoute = '/';
 export const dashboardRoute = '/dashboard';
--- src/routes/dashboard/to-do/to-do-row/ToDoRow.svelte
+++ src/routes/dashboard/to-do/to-do-row/ToDoRow.svelte
@@ -3,6 +3,8 @@
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import Button from '$lib/components/button/Button.svelte';
 	import type { EEvent } from '$lib/event/utils';
+	import { DATETIME } from '$lib/utils';
+	import { format } from 'date-fns';
 	import { createEventDispatcher } from 'svelte';
 
 	export let toDo: EEvent;
@@ -11,8 +13,13 @@
 </script>
 
 <li class="flex justify-between gap-x-6 py-3">
-	<div class="text-sm font-semibold leading-6 text-gray-900">{toDo.name}</div>
-
+	<div class="text-sm font-semibold leading-6 text-gray-900 flex-1">{toDo.name}</div>
+	<div class="text-sm font-semibold leading-6 text-gray-900">
+		{format(toDo.startDate, DATETIME)}
+	</div>
+	<div class="text-sm font-semibold leading-6 text-gray-900">
+		{format(toDo.endDate, DATETIME)}
+	</div>
 	<div>
 		<Button on:click={() => dispatch('edit', toDo)} type="button">
 			<Icon src={Settings} class="h-4 w-4" />
