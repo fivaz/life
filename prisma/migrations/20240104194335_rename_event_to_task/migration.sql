ed9e898f2be73857437789db7012c027a41e01d7
fivaz <s.a.fivaz@gmail.com>
2024-01-05 12:26:38 -0300

    Revert "rename table Event to Task"
    This reverts commit c8911fd2430f4ccc926952d9fd1b48ac05aa9db0.

--- prisma/migrations/20240104194335_rename_event_to_task/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "Event" RENAME CONSTRAINT "Event_categoryId_fkey" TO "Task_categoryId_fkey";
-ALTER TABLE "Event" RENAME CONSTRAINT "Event_userId_fkey" TO "Task_userId_fkey";
-ALTER TABLE "Event" RENAME TO "Task";
