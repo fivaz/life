8b874d17d4ffa6a61d62240c8b2d8d94d92889e2
fivaz <s.a.fivaz@gmail.com>
2024-01-05 12:26:11 -0300

    Revert commits to try to fix the issue with Prisma in Vercel
    Revert "change sveltejs adapter"
    This reverts commit f7b84caf863c219acdf3721fe0bf681d9e020c42.
    
    Revert "add a resolver for prisma in vite config"
    This reverts commit 162fcbd4086a579137a9e915b89e655abd1ffffa.
    
    Revert "add a script to copy prisma files before build"
    This reverts commit 0f32b1aaccfc4587a90885041ee56a73adea8c3f.
    
    Revert "remove script"
    This reverts commit 75d6a4e62d7df510820ff50414201851b0d88869.

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,4 @@ export default defineConfig({
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
-	// // fix the issue https://github.com/prisma/prisma/issues/12504
-	// resolve: {
-	// 	alias: {
-	// 		'.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
-	// 	},
-	// },
 });
