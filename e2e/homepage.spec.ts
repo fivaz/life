e21e3dcf9fbc7d7444c9b7c33218c4de55aadcb0
fivaz <s.a.fivaz@gmail.com>
2024-01-07 02:14:17 -0300

    change url in homepage test

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
-import { homeRoute } from '../src/lib/utils';
+import { loginRoute } from '../src/lib/utils';
 
 test('the user can navigate to the homepage', async ({ page }) => {
-	await page.goto(homeRoute);
+	await page.goto(loginRoute);
 
 	const createEventButton = page.getByRole('button', { name: 'create event' });
 	await expect(createEventButton).toBeVisible();
