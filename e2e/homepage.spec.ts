fca3c7f1270f112d8bcd78d89714e7661b4e2d6d
fivaz <s.a.fivaz@gmail.com>
2024-01-07 00:48:49 -0300

    sending github secrets as environment variables of a github job

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
-import { loginRoute } from '../src/lib/utils';
+import { homeRoute } from '../src/lib/utils';
 
 test('the user can navigate to the homepage', async ({ page }) => {
-	await page.goto(loginRoute);
+	await page.goto(homeRoute);
 
 	const createEventButton = page.getByRole('button', { name: 'create event' });
 	await expect(createEventButton).toBeVisible();
