6643a757844e8aa218bd289e7b80a14114aef040
fivaz <s.a.fivaz@gmail.com>
2024-01-09 00:42:08 -0300

    change migration name

--- tests/test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-import { homeRoute } from '$lib/consts';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto(homeRoute);
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
-});
