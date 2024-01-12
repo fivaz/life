a0e4a3af3a59bfaee71cfaa282ee0582ad2bbe7a
fivaz <s.a.fivaz@gmail.com>
2024-01-12 10:41:05 -0300

    add component Alert

---
+++ src/lib/components/alert/alert.stories.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+	import Alert from './Alert.svelte';
+
+	export const meta = {
+		component: Alert,
+		tags: ['autodocs'],
+		parameters: {
+			layout: 'centered',
+		},
+	} satisfies Meta<Alert>;
+</script>
+
+<Template let:args>
+	<Alert {...args} />
+</Template>
+
+<Story name="Primary" args={{}} />
