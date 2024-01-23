18b70a7dcb22d9ffc10588224f9687434d73e55a
fivaz <s.a.fivaz@gmail.com>
2024-01-22 21:44:08 -0300

    add close button to form

--- src/lib/components/task-form/TaskForm.svelte
+++ src/lib/components/task-form/TaskForm.svelte
@@ -7,14 +7,12 @@
 	import DefaultSection from '$lib/components/task-form/default-section/DefaultSection.svelte';
 	import EventSection from '$lib/components/task-form/event-section/EventSection.svelte';
 	import type { ActionData } from '../../../../.svelte-kit/types/src/routes/dashboard/home/$types';
-	import {
-		handleCreate,
-		handleDelete,
-		handleEdit,
-		isEventsDateInverted,
-	} from './service';
+	import { handleCreate, handleDelete, handleEdit, isEventsDateInverted } from './service';
 	import type { TaskIn } from './service';
 	import 'flatpickr/dist/themes/airbnb.css';
+	import { XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { closeModal } from '$lib/form-modal/store';
 
 	export let form: ActionData | null;
 
@@ -60,9 +58,21 @@
 	class="w-[355px] shadow rounded-md overflow-hidden relative"
 >
 	<div class="flex flex-col gap-2 px-4 py-5 bg-neutral-100 sm:p-6">
-		<h2 class="text-lg font-medium text-gray-900">
-			{formName}
-		</h2>
+		<div class="flex justify-between items-center">
+			<h2 class="text-lg font-medium text-gray-900">
+				{formName}
+			</h2>
+			<div class="pl-2">
+				<button
+					type="button"
+					class="inline-flex rounded-md p-1.5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2 focus:ring-offset-gray-50"
+					on:click={() => closeModal()}
+				>
+					<span class="sr-only">Dismiss</span>
+					<Icon src={XMark} class="h-5 w-5" aria-hidden="true" />
+				</button>
+			</div>
+		</div>
 
 		<Alert type="error" isVisible={!!error} hasCloseButton={false}>{error}</Alert>
 
