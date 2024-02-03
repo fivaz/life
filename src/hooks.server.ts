5add531b25146a7c41d1a5b0ae71da8c030636c9
fivaz <s.a.fivaz@gmail.com>
2024-02-03 01:36:09 +0100

    remove server stuff

--- src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import { auth } from '$lib/server/lucia';
-
-export const handle: Handle = async ({ event, resolve }) => {
-	// we can pass `event` because we used the SvelteKit middleware
-	event.locals.auth = auth.handleRequest(event);
-	return resolve(event);
-};
--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,20 +1,7 @@
 /// <reference types="lucia" />
 declare global {
-	namespace Lucia {
-		type Auth = import('$lib/server/lucia').Auth;
-		type DatabaseUserAttributes = {
-			avatar: string;
-			name: string;
-			username: string;
-		};
-		// eslint-disable-next-line @typescript-eslint/ban-types
-		type DatabaseSessionAttributes = {};
-	}
-
 	namespace App {
-		interface Locals {
-			auth: import('lucia').AuthRequest;
-		}
+		interface Locals {}
 	}
 }
 
