42c149a1a291c6fdd39847ce4c867c717f1919d6
fivaz <s.a.fivaz@gmail.com>
2024-02-03 06:00:21 +0100

    fix style of checkbox

--- src/routes/dashboard/home/calendar/calendar-body/calendar-columns/calendar-rows/event-cell/EventCell.svelte
+++ src/routes/dashboard/home/calendar/calendar-body/calendar-columns/calendar-rows/event-cell/EventCell.svelte
@@ -21,52 +21,54 @@
 	const dispatch = createEventDispatcher<{ edit: { event: Event; targetDate: Date } }>();
 </script>
 
-<div
-	on:dragstart={(e) => dragStart(e, event)}
-	on:dragend={(e) => dragEnd(e)}
-	on:click={() => dispatch('edit', { event, targetDate })}
-	on:keydown={(e) => {
-		if (e.key === 'Enter') {
-			dispatch('edit', { event, targetDate });
-		}
-	}}
-	tabindex="0"
-	role="button"
-	draggable="true"
-	class={classnames(
-		{ 'py-2': !isShort(event) },
-		'cursor-move group w-full h-full flex flex-col rounded-lg py-1 px-2 text-xs leading-5',
-		tailwindColors[event.category.color].lightBg,
-		tailwindColors[event.category.color].hoverBg,
-		tailwindColors[event.category.color].text,
-	)}
->
-	<SignedIn let:user>
-		<input
-			type="checkbox"
-			checked={event.isDone}
-			on:change={() => toggleCompletion(user.uid, event, targetDate)}
-			on:click|stopPropagation
-			disabled={loading}
-			class="rounded border-gray-300 focus:ring-indigo-600"
-		/>
-	</SignedIn>
-	<p class={classnames({ hidden: isShort(event) }, 'text-blue-500 group-hover:text-blue-700')}>
-		<time dateTime={`${event.date} ${event.startTime}`}>
-			{format(parse(event.startTime, TIME, new Date()), 'p')}
-		</time>
-	</p>
-
-	<EventName {event} />
-
-	<p
+<SignedIn let:user>
+	<div
+		on:dragstart={(e) => dragStart(e, event)}
+		on:dragend={(e) => dragEnd(e)}
+		on:click={() => dispatch('edit', { event, targetDate })}
+		on:keydown={(e) => {
+			if (e.key === 'Enter') {
+				dispatch('edit', { event, targetDate });
+			}
+		}}
+		tabindex="0"
+		role="button"
+		draggable="true"
 		class={classnames(
-			{ hidden: isShort(event) || !event.description },
-			'text-pink-500 group-hover:text-pink-700',
+			{ 'py-2': !isShort(event) },
+			'cursor-move group w-full h-full flex flex-col rounded-lg py-1 px-2 text-xs leading-5',
+			tailwindColors[event.category.color].lightBg,
+			tailwindColors[event.category.color].hoverBg,
+			tailwindColors[event.category.color].text,
 		)}
 	>
-		{event.description}
-	</p>
-</div>
+		<div class="absolute right-0 pr-2">
+			<input
+				type="checkbox"
+				checked={event.isDone}
+				on:change={() => toggleCompletion(user.uid, event, targetDate)}
+				on:click|stopPropagation
+				disabled={loading}
+				class="rounded border-gray-300 focus:ring-indigo-600"
+			/>
+		</div>
+		<p class={classnames({ hidden: isShort(event) }, 'text-blue-500 group-hover:text-blue-700')}>
+			<time dateTime={`${event.date} ${event.startTime}`}>
+				{format(parse(event.startTime, TIME, new Date()), 'p')}
+			</time>
+		</p>
+
+		<EventName {event} />
+
+		<p
+			class={classnames(
+				{ hidden: isShort(event) || !event.description },
+				'text-pink-500 group-hover:text-pink-700',
+			)}
+		>
+			{event.description}
+		</p>
+	</div>
+</SignedIn>
 
 <Loading {loading} class={classnames('h-4 w-4', tailwindColors[event.category.color].text)} />
