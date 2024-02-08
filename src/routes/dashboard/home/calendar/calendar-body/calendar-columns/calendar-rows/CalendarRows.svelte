b1ae98cb5b98f33771e769ea5c9b48de9e01039f
fivaz <s.a.fivaz@gmail.com>
2024-02-08 07:07:37 +0100

    implement drag and drop with mouse events

--- src/routes/dashboard/home/calendar/calendar-body/calendar-columns/calendar-rows/CalendarRows.svelte
+++ src/routes/dashboard/home/calendar/calendar-body/calendar-columns/calendar-rows/CalendarRows.svelte
@@ -10,28 +10,27 @@
 	export let targetDate: Date;
 
 	const quarterHourInterval = halfHourInterval * 2;
+
+	let container: HTMLDivElement | undefined = undefined;
 </script>
 
 <!--TODO make top and side sticky-->
-<!--TODO handle toDo that takes more than 1 day-->
+<!--TODO handle tasks that takes more than 1 day-->
 
 <div class="relative w-full h-full">
 	<CalendarGrid on:create on:move />
 	<!--pointer-events-none will make the list container of events incapable of catching point events-->
 	<!--so we can catch the events emit by CalendarGrid behind it-->
-	<ol
+	<div
+		bind:this={container}
 		class="absolute inset-0 grid pointer-events-none"
 		style="grid-template-rows: repeat({quarterHourInterval}, minmax(1.75rem, 1fr)) auto"
 	>
 		{#each events as event (event)}
 			<!--then we set each element of the list with pointer-events-auto so they can catch events to edit the TEvent-->
 			<!--the min-w-0 is necessary due to a issue with truncate text in a grid layout see: https://dev.to/timhecker/grid-cell-issue-with-white-space-nowrap--text-overflow-ellipsis-52g6-->
-			<li
-				class="relative w-full h-full rounded-lg pointer-events-auto min-w-0"
-				style={getGridRowsStyle(event)}
-			>
-				<EventCell {event} on:edit {targetDate} />
-			</li>
+
+			<EventCell {container} {event} on:edit {targetDate} />
 		{/each}
-	</ol>
+	</div>
 </div>
--- src/routes/dashboard/home/calendar/calendar-body/calendar-columns/calendar-rows/event-cell/EventCell.svelte
+++ src/routes/dashboard/home/calendar/calendar-body/calendar-columns/calendar-rows/event-cell/EventCell.svelte
@@ -9,8 +9,11 @@
 	import { createEventDispatcher } from 'svelte';
 	import { SignedIn } from 'sveltefire';
 
+	import { getGridRowsStyle } from '../service';
 	import EventName from './event-name/EventName.svelte';
 	import { dragEnd, dragStart, isShort, toggleCompletion } from './service';
+
+	export let container: HTMLDivElement | undefined;
 
 	export let event: AnyEvent;
 
@@ -18,58 +21,137 @@
 	// this might not be the same day as the event.startDate
 	export let targetDate: Date;
 
+	let draggedElement: HTMLDivElement | null = null;
+
+	let dragging = false;
+	let startX = 0;
+	let startY = 0;
+	let x = 0;
+	let y = 0;
+
 	let loading = false;
 
+	const cellSize = 50;
+
 	const dispatch = createEventDispatcher<{ edit: { event: Event; targetDate: Date } }>();
+
+	function calculateCellPosition() {
+		const cellX = Math.floor(x / cellSize);
+		const cellY = Math.floor(y / cellSize);
+		return { cellX, cellY };
+	}
+
+	function handleMouseDown(event: MouseEvent) {
+		dragging = true;
+		startX = event.clientX - x;
+		startY = event.clientY - y;
+	}
+
+	function handleMouseMove(event: MouseEvent) {
+		if (dragging && draggedElement) {
+			x = event.clientX - startX;
+			y = event.clientY - startY;
+
+			// console.log(event.clientX);
+			// console.log(event.clientY);
+
+			// Calculate the cell position while dragging
+			// const { cellX, cellY } = calculateCellPosition();
+			// console.log(`Element is in cell (${cellX}, ${cellY})`);
+		}
+	}
+
+	function roundToNearest15Minutes(minutes: number) {
+		return Math.round(minutes / 15) * 15;
+	}
+
+	function updateEventTime(draggedEvent: HTMLDivElement, timeTab: HTMLDivElement) {
+		// Get the height of the time tab
+		const timeTabHeight = timeTab.clientHeight;
+
+		// Get the top position of the dragged event relative to the time tab
+		const eventTop = draggedEvent.getBoundingClientRect().top - timeTab.getBoundingClientRect().top;
+
+		// Calculate the percentage of the day based on the event's position
+		const percentageOfDay = (eventTop / timeTabHeight) * 100;
+
+		// Calculate the time in minutes
+		const totalMinutes = Math.round((percentageOfDay * 24 * 60) / 100);
+
+		// Round to the nearest 15 minutes interval
+		const roundedMinutes = roundToNearest15Minutes(totalMinutes);
+
+		// Convert total minutes to hours and minutes
+		const hours = Math.floor(roundedMinutes / 60);
+		const minutes = roundedMinutes % 60;
+
+		// Update your event with the calculated time
+		console.log(`Event placed at ${hours}:${minutes}`);
+	}
+
+	function handleMouseUp(event: MouseEvent) {
+		if (!dragging || !draggedElement || !container) {
+			return;
+		}
+		updateEventTime(draggedElement, container);
+		dragging = false;
+	}
 </script>
 
+<!--getBoundingClientRect-->
 <SignedIn let:user>
 	<div
+		bind:this={draggedElement}
 		class={classnames(
-			{ 'py-2': !isShort(event) },
-			'cursor-move group w-full h-full flex flex-col rounded-lg py-1 px-2 text-xs leading-5',
+			'w-full h-full rounded-lg pointer-events-auto min-w-0',
+			'cursor-grab select-none absolute',
+			{ 'py-2': !isShort(event), 'z-10': dragging },
+			'group w-full h-full flex flex-col rounded-lg py-1 px-2 text-xs leading-5',
 			tailwindColors[event.category.color].lightBg,
 			tailwindColors[event.category.color].hoverBg,
 			tailwindColors[event.category.color].text,
 		)}
-		draggable="true"
-		on:click={() => dispatch('edit', { event, targetDate })}
-		on:dragend={(e) => dragEnd(e)}
-		on:dragstart={(e) => dragStart(e, event)}
 		on:keydown={(e) => {
 			if (e.key === 'Enter') {
 				dispatch('edit', { event, targetDate });
 			}
 		}}
+		on:mousedown={handleMouseDown}
+		on:mouseleave={handleMouseUp}
+		on:mousemove={handleMouseMove}
+		on:mouseup={handleMouseUp}
 		role="button"
+		style={`transform: translate(${x}px, ${y}px); ${getGridRowsStyle(event)}`}
 		tabindex="0"
 	>
-		<div class="absolute right-0 pr-2">
-			<input
-				checked={event.isDone}
-				class="rounded border-gray-300 focus:ring-indigo-600"
-				disabled={loading}
-				on:change={() => toggleCompletion(user.uid, event, targetDate)}
-				on:click|stopPropagation
-				type="checkbox"
-			/>
+		<div class="relative">
+			<div class="absolute right-0 pr-2">
+				<input
+					checked={event.isDone}
+					class="rounded border-gray-300 focus:ring-indigo-600"
+					disabled={loading}
+					on:change={() => toggleCompletion(user.uid, event, targetDate)}
+					on:click|stopPropagation
+					type="checkbox"
+				/>
+			</div>
+			<p class={classnames({ hidden: isShort(event) }, 'text-blue-500 group-hover:text-blue-700')}>
+				<time dateTime={`${event.date} ${event.startTime}`}>
+					{format(parse(event.startTime, TIME, new Date()), 'p')}
+				</time>
+			</p>
+
+			<EventName {event} />
+
+			<p
+				class={classnames(
+					{ hidden: isShort(event) || !event.description },
+					'text-pink-500 group-hover:text-pink-700',
+				)}
+			>
+				{event.description}
+			</p>
 		</div>
-		<p class={classnames({ hidden: isShort(event) }, 'text-blue-500 group-hover:text-blue-700')}>
-			<time dateTime={`${event.date} ${event.startTime}`}>
-				{format(parse(event.startTime, TIME, new Date()), 'p')}
-			</time>
-		</p>
-
-		<EventName {event} />
-
-		<p
-			class={classnames(
-				{ hidden: isShort(event) || !event.description },
-				'text-pink-500 group-hover:text-pink-700',
-			)}
-		>
-			{event.description}
-		</p>
 	</div>
 </SignedIn>
 
