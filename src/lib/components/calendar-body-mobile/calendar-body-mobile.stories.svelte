6eed78dfe33a32a30b0859d7e73d41e3b46e6131
work <s.a.fivaz@gmail.com>
2024-04-18 18:00:27 +0200

    wip - make Calendar Header sticky

---
+++ src/lib/components/calendar-body-mobile/calendar-body-mobile.stories.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	import type { Meta } from '@storybook/svelte';
+
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+
+	import CalendarBodyMobile from './CalendarBodyMobile.svelte';
+
+	export const meta = {
+		component: CalendarBodyMobile,
+		parameters: {
+			layout: 'centered',
+		},
+		tags: ['autodocs'],
+	} satisfies Meta<CalendarBodyMobile>;
+</script>
+
+<Template let:args>
+	<div class="h-96">
+		<CalendarBodyMobile {...args}></CalendarBodyMobile>
+	</div>
+</Template>
+
+<Story args={{}} name="Primary" />
