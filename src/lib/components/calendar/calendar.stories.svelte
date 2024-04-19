f9c7d7cfd6259d834aefcab533397a7c6d8e06f7
work <s.a.fivaz@gmail.com>
2024-04-19 23:54:59 +0200

    wip - mock firebase

--- src/lib/components/calendar/calendar.stories.svelte
+++ src/lib/components/calendar/calendar.stories.svelte
@@ -1,8 +1,6 @@
 <script context="module" lang="ts">
 	import type { Meta } from '@storybook/svelte';
 
-	import { categories } from '$lib/category/seed';
-	import { events } from '$lib/task/seed';
 	import { Story, Template } from '@storybook/addon-svelte-csf';
 
 	import Calendar from './Calendar.svelte';
@@ -13,12 +11,24 @@
 			layout: 'fullscreen',
 		},
 	} satisfies Meta<Calendar>;
+
+	import { connectAuthEmulator, getAuth } from 'firebase/auth';
+	import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+	import { FirebaseApp } from 'sveltefire';
+
+	const db = getFirestore();
+	connectFirestoreEmulator(db, '127.0.0.1', 8080);
+
+	const auth = getAuth();
+	connectAuthEmulator(auth, 'http://127.0.0.1:9099');
 </script>
 
 <Template let:args>
-	<div class="h-screen">
-		<Calendar {...args} {categories} {events} />
-	</div>
+	<FirebaseApp {auth} firestore={db}>
+		<div class="h-screen">
+			<Calendar {...args} />
+		</div>
+	</FirebaseApp>
 </Template>
 
 <Story args={{}} name="Primary" />
