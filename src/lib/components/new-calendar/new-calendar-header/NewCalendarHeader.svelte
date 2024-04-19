08361ecf2b9eb77ebe36968fcef1b081d7a6380a
work <s.a.fivaz@gmail.com>
2024-04-19 10:27:25 +0200

    wip - make Calendar Header sticky

--- src/lib/components/new-calendar/new-calendar-header/NewCalendarHeader.svelte
+++ src/lib/components/new-calendar/new-calendar-header/NewCalendarHeader.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { DATE } from '$lib/consts';
 	import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@rgossiaux/svelte-headlessui';
-	import { ChevronDown, ChevronLeft, ChevronRight, EllipsisHorizontal } from '@steeze-ui/heroicons';
+	import { ChevronLeft, ChevronRight, EllipsisHorizontal } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { clsx } from 'clsx';
 	import { addDays, format, startOfWeek } from 'date-fns';
@@ -10,8 +10,6 @@
 	export let weekStart: Date;
 
 	let currentDate = new Date();
-
-	const viewControls = [{ text: 'Year view' }, { text: 'Week view' }, { text: 'Month view' }];
 
 	const dispatch = createEventDispatcher();
 
@@ -53,6 +51,7 @@
 				<span class="sr-only">Previous week</span>
 				<Icon aria-hidden="true" class="h-5 w-5" src={ChevronLeft} />
 			</button>
+
 			<button
 				class="hidden border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block"
 				on:click={goToToday}
@@ -60,7 +59,9 @@
 			>
 				Today
 			</button>
+
 			<span class="relative -mx-px h-5 w-px bg-gray-300 md:hidden" />
+
 			<button
 				class="flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50"
 				on:click={goToNextWeek}
@@ -72,46 +73,8 @@
 		</div>
 
 		<div class="hidden md:ml-4 md:flex md:items-center">
-			<Menu class="relative">
-				<MenuButton
-					class="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-					type="button"
-				>
-					Day view
-					<Icon aria-hidden="true" class="-mr-1 h-5 w-5 text-gray-400" src={ChevronDown} />
-				</MenuButton>
+			<div class="ml-3 h-6 w-px bg-gray-300" />
 
-				<Transition
-					enter="transition ease-out duration-100"
-					enterFrom="transform opacity-0 scale-95"
-					enterTo="transform opacity-100 scale-100"
-					leave="transition ease-in duration-75"
-					leaveFrom="transform opacity-100 scale-100"
-					leaveTo="transform opacity-0 scale-95"
-				>
-					<MenuItems
-						class="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-					>
-						<div class="py-1">
-							{#each viewControls as viewControl (viewControl.text)}
-								<MenuItem let:active>
-									<button
-										class={clsx(
-											// eslint-disable-next-line sonarjs/no-duplicate-string
-											active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-											// eslint-disable-next-line sonarjs/no-duplicate-string
-											'block px-4 py-2 text-sm',
-										)}
-									>
-										{viewControl.text}
-									</button>
-								</MenuItem>
-							{/each}
-						</div>
-					</MenuItems>
-				</Transition>
-			</Menu>
-			<div class="ml-6 h-6 w-px bg-gray-300" />
 			<button
 				class="ml-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 				on:click={createEvent}
@@ -158,27 +121,13 @@
 							<button
 								class={clsx(
 									active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-									'block px-4 py-2 text-sm',
+									'block px-4 py-2 text-sm w-full text-left',
 								)}
 								on:click={goToToday}
 							>
 								Go to today
 							</button>
 						</MenuItem>
-					</div>
-					<div class="py-1">
-						{#each viewControls as viewControl (viewControl.text)}
-							<MenuItem let:active>
-								<button
-									class={clsx(
-										active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-										'block px-4 py-2 text-sm',
-									)}
-								>
-									{viewControl.text}
-								</button>
-							</MenuItem>
-						{/each}
 					</div>
 				</MenuItems>
 			</Transition>
