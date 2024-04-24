222c138760578899acdb8582bef589f1df0081e8
work <s.a.fivaz@gmail.com>
2024-04-24 22:11:36 +0200

    add collapsable component

---
+++ src/lib/components/collapsable/Collapsable.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import {
+		Disclosure,
+		DisclosureButton,
+		DisclosurePanel,
+		Transition,
+	} from '@rgossiaux/svelte-headlessui';
+	import { ChevronRight } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+
+	export let title: string;
+</script>
+
+<Disclosure class="rounded-lg bg-white p-2" defaultOpen let:open>
+	<DisclosureButton class="flex w-full justify-between">
+		<span>{title}</span>
+		<Icon class="h-5 w-5 {open ? 'rotate-90 transform' : ''}" src={ChevronRight} />
+	</DisclosureButton>
+	<Transition
+		class="overflow-hidden transition-all duration-500"
+		enterFrom="transform opacity-0 max-h-0"
+		enterTo="transform opacity-100 max-h-36"
+		leaveFrom="transform opacity-100 max-h-36"
+		leaveTo="transform opacity-0 max-h-0"
+	>
+		<DisclosurePanel class="flex flex-col gap-2 pt-2 text-gray-500">
+			<slot />
+		</DisclosurePanel>
+	</Transition>
+</Disclosure>
---
+++ src/lib/components/collapsable/collapsable.stories.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	import type { Meta } from '@storybook/svelte';
+
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+
+	import Collapsable from './Collapsable.svelte';
+
+	export const meta = {
+		argTypes: {},
+		component: Collapsable,
+		parameters: {
+			layout: 'fullscreen',
+		},
+	} satisfies Meta<Collapsable>;
+</script>
+
+<Template let:args>
+	<Collapsable {...args} title="Test">
+		<div class="h-96 w-96 bg-indigo-100" />
+	</Collapsable>
+</Template>
+
+<Story args={{}} name="Primary" />
