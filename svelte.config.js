a43d1e151b68f02776d4053002a727fa29b9b5b3
fivaz <s.a.fivaz@gmail.com>
2024-06-23 01:14:44 +0200

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-static';
 // import adapter from 'svelte-adapter-firebase';
-// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
