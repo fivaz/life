9c9991039e57277e803ee499f8665c998fcaa4ef
fivaz <s.a.fivaz@gmail.com>
2024-06-23 01:06:52 +0200

    simplify home page access

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { homeRoute, loginRoute } from '$lib/consts';
-	import { auth } from '$lib/firebase';
-	import { Loader2 } from '@steeze-ui/lucide-icons';
-	import { Icon } from '@steeze-ui/svelte-icon';
-	import { onAuthStateChanged } from 'firebase/auth';
+	import { homeRoute } from '$lib/consts';
 	import { onMount } from 'svelte';
 
-	onMount(() => {
-		onAuthStateChanged(auth, (user) => {
-			goto(user ? homeRoute : loginRoute);
-		});
-	});
+	onMount(() => goto(homeRoute));
 </script>
-
-<div class="flex h-screen items-center justify-center">
-	<div class="flex gap-3">
-		<Icon class="h-6 w-6 animate-spin" src={Loader2} />
-		<p class="font-semibold">Loading...</p>
-	</div>
-</div>
