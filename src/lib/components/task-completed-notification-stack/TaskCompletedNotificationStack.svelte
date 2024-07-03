38062e7628faf1545a8ab9747b5efe32f14a8d43
work <s.a.fivaz@gmail.com>
2024-07-03 22:22:10 +0200

    remove store for notifications

--- src/lib/components/task-completed-notification-stack/TaskCompletedNotificationStack.svelte
+++ src/lib/components/task-completed-notification-stack/TaskCompletedNotificationStack.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { AnyTask } from '$lib/task/utils';
 
-	import TaskCompletedNotification from '$lib/components/task-completed-notification/TaskCompletedNotification.svelte';
+	import TaskCompletedNotification from '$lib/components/task-completed-notification-stack/task-completed-notification/TaskCompletedNotification.svelte';
 	import { SignedIn } from 'sveltefire';
 
 	export let completedTasks: AnyTask[] = [];
--- src/lib/components/task-completed-notification-stack/service.ts
+++
@@ -1,9 +0,0 @@
-import { type AnyTask } from '$lib/task/utils';
-import { writable } from 'svelte/store';
-
-export const notifications = writable<AnyTask[]>([]);
-
-export function addNotification(task: AnyTask) {
-	const clonedTask = JSON.parse(JSON.stringify(task));
-	notifications.update((tasks) => [...tasks, clonedTask]);
-}
