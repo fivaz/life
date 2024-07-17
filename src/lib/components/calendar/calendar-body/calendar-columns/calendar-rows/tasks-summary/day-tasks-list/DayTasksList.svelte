d629c7bb900ee90452012ebd9918db1e595f8ed6
work <s.a.fivaz@gmail.com>
2024-07-17 06:08:15 +0200

    add scroll to DayTaskList

--- src/lib/components/calendar/calendar-body/calendar-columns/calendar-rows/tasks-summary/day-tasks-list/DayTasksList.svelte
+++ src/lib/components/calendar/calendar-body/calendar-columns/calendar-rows/tasks-summary/day-tasks-list/DayTasksList.svelte
@@ -42,64 +42,63 @@
 	}
 </script>
 
-<div class="relative w-11/12 max-w-[543px] text-sm leading-6 text-gray-900">
-	<h2 class="sr-only">To Dos</h2>
-	<div class="divide-y divide-gray-900/5 rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
-		<div class="p-6 font-semibold">
-			<div>Pending Tasks</div>
-			{#if uncompletedTasks.length}
-				<div class="mt-1"><span class="text-base">{totalDuration}</span> to complete</div>
-			{/if}
-		</div>
-
-		<ul class="flex flex-col py-3">
-			{#each tasks as task, index (task)}
-				<!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
-				<li
-					class="flex cursor-pointer items-center px-6 py-3 hover:bg-gray-100 hover:underline"
-					on:click={() => dispatch('editTask', { targetDate: '', task: task })}
-				>
-					<span class="w-5 pr-3 font-medium text-gray-500">{index + 1}</span>
-					<Icon
-						class="h-5 w-8 pr-3 text-gray-400"
-						src={isToDo(task) ? CalendarDays : CalendarClock}
-						theme="solid"
-					/>
-					<div
-						class={clsx(
-							{ 'line-through': task.isDone },
-							'flex-1 truncate pr-3 font-medium text-gray-500',
-						)}
-					>
-						{task.name}
-					</div>
-
-					{#if task.goal?.icon}
-						<GoalIcon class="h-4 w-8 pr-3 text-gray-400" name={task.goal.icon} />
-					{/if}
-					<div class="w-12">{getDuration(task)}</div>
-					<div class="w-16">
-						<div
-							class={clsx(
-								task.isDone
-									? 'bg-green-50 text-green-700 ring-green-600/20'
-									: 'bg-red-50 text-red-700 ring-red-600/20',
-								'm-auto w-max rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset',
-							)}
-						>
-							{task.isDone ? 'Done' : 'Undone'}
-						</div>
-					</div>
-				</li>
-			{/each}
-		</ul>
-
+<div
+	class="relative flex h-auto max-h-[90%] w-11/12 max-w-[543px] flex-col divide-y divide-gray-900/5 rounded-lg bg-gray-50 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/5"
+>
+	<div class="flex-none p-6 font-semibold">
+		<div>Pending Tasks</div>
 		{#if uncompletedTasks.length}
-			<div class="p-6">
-				<button class="font-semibold hover:underline" on:click={postponeToDos}>
-					Postpone remaining unset tasks <span aria-hidden="true">&rarr;</span>
-				</button>
-			</div>
+			<div class="mt-1"><span class="text-base">{totalDuration}</span> to complete</div>
 		{/if}
 	</div>
+
+	<ul class="flex flex-grow flex-col overflow-y-auto py-3">
+		{#each tasks as task, index (task)}
+			<!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
+			<li
+				class="flex cursor-pointer items-center px-6 py-3 hover:bg-gray-100 hover:underline"
+				on:click={() => dispatch('editTask', { targetDate: '', task: task })}
+			>
+				<span class="w-5 pr-3 font-medium text-gray-500">{index + 1}</span>
+				<Icon
+					class="h-5 w-8 pr-3 text-gray-400"
+					src={isToDo(task) ? CalendarDays : CalendarClock}
+					theme="solid"
+				/>
+				<div
+					class={clsx(
+						{ 'line-through': task.isDone },
+						'flex-1 truncate pr-3 font-medium text-gray-500',
+					)}
+				>
+					{task.name}
+				</div>
+
+				{#if task.goal?.icon}
+					<GoalIcon class="h-4 w-8 pr-3 text-gray-400" name={task.goal.icon} />
+				{/if}
+				<div class="w-12">{getDuration(task)}</div>
+				<div class="w-16">
+					<div
+						class={clsx(
+							task.isDone
+								? 'bg-green-50 text-green-700 ring-green-600/20'
+								: 'bg-red-50 text-red-700 ring-red-600/20',
+							'm-auto w-max rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset',
+						)}
+					>
+						{task.isDone ? 'Done' : 'Undone'}
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+
+	{#if uncompletedTasks.length}
+		<div class="flex-none p-6">
+			<button class="font-semibold hover:underline" on:click={postponeToDos}>
+				Postpone remaining unset tasks <span aria-hidden="true">&rarr;</span>
+			</button>
+		</div>
+	{/if}
 </div>
