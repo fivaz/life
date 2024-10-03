7b0cbd0dcd42dabc5b36f9f1fdf42d285731f9d6
fivaz <s.a.fivaz@gmail.com>
2024-10-03 22:02:02 +0200

    increase size of grid height

--- src/lib/components/calendar/calendar-body/calendar-columns/calendar-rows/calendar-grid/service.ts
+++ src/lib/components/calendar/calendar-body/calendar-columns/calendar-rows/calendar-grid/service.ts
@@ -1,11 +1,13 @@
 import { writable } from 'svelte/store';
 
-export const GRID_CLASS = 'grid-class';
 // this var is used in a style tag ass grid-class, if you change the value, remember to also change the style tag
+export const GRID_CLASS = 'grid-class';
 export const GRID_CELL_TIME = 15;
 
+// 24 - hours
+// 60 - minutes
 export const NUMBER_OF_CELLS = (24 * 60) / GRID_CELL_TIME;
 
-export const GRID_CELL_HEIGHT = 30;
+export const GRID_CELL_HEIGHT = 60;
 
 export const isSomethingDragging = writable<boolean>(false);
