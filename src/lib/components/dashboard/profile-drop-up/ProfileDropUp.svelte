e871cd20135bcd95f05d2e5706fe007bb62edf58
fivaz <s.a.fivaz@gmail.com>
2024-10-25 18:53:47 +0200

    remove svelte-headlessui from ProfileDropUp

--- src/lib/components/dashboard/profile-drop-up/ProfileDropUp.svelte
+++ src/lib/components/dashboard/profile-drop-up/ProfileDropUp.svelte
@@ -2,41 +2,52 @@
 	import { goto } from '$app/navigation';
 	import { Routes } from '$lib/consts';
 	import { auth } from '$lib/firebase';
-	import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@rgossiaux/svelte-headlessui';
-	import { clsx } from 'clsx';
 	import { signOut } from 'firebase/auth';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	let isOpen = false;
+
+	function handleClickOutside() {
+		if (isOpen) {
+			isOpen = false;
+		}
+	}
+
+	onMount(() => {
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	});
 </script>
 
-<Menu as="div" class="relative inline-block text-left">
-	<Transition
-		enter="transition ease-out duration-50"
-		enterFrom="transform opacity-0 scale-95"
-		enterTo="transform opacity-100 scale-100"
-		leave="transition ease-in duration-75"
-		leaveFrom="transform opacity-100 scale-100"
-		leaveTo="transform opacity-0 scale-95"
-	>
-		<MenuItems
+<div class="relative inline-block text-left">
+	<!--	<div-->
+	<!--		enter="transition ease-out duration-50"-->
+	<!--		enterFrom="transform opacity-0 scale-95"-->
+	<!--		enterTo="transform opacity-100 scale-100"-->
+	<!--		leave="transition ease-in duration-75"-->
+	<!--		leaveFrom="transform opacity-100 scale-100"-->
+	<!--		leaveTo="transform opacity-0 scale-95"-->
+	<!--	>-->
+	{#if isOpen}
+		<ul
 			class="absolute -top-2 left-0 w-32 origin-top-right -translate-y-full transform divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+			transition:fade
 		>
-			<MenuItem let:active>
+			<li>
 				<a
-					class={clsx(
-						active ? 'bg-gray-50' : '',
-						'block px-3 py-1 text-sm leading-6 text-gray-900',
-					)}
+					class="block px-3 py-1 text-sm leading-6 text-gray-900 hover:bg-gray-50"
 					href={Routes.PROFILE}
 					on:click
 				>
 					Profile
 				</a>
-			</MenuItem>
-			<MenuItem let:active>
+			</li>
+			<li>
 				<button
-					class={clsx(
-						active ? 'bg-gray-50' : '',
-						'block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900',
-					)}
+					class="block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900 hover:bg-gray-50"
 					on:click={async () => {
 						await signOut(auth);
 						void goto(Routes.LOGIN);
@@ -44,10 +55,11 @@
 				>
 					Sign out
 				</button>
-			</MenuItem>
-		</MenuItems>
-	</Transition>
-	<MenuButton class="w-full">
+			</li>
+		</ul>
+		<!--		</div>-->
+	{/if}
+	<button class="w-full" on:click|stopPropagation={() => (isOpen = !isOpen)}>
 		<slot />
-	</MenuButton>
-</Menu>
+	</button>
+</div>
