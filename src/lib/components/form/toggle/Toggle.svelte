9806c43edebe9f44d6a11cd33eddabe9d4f45502
fivaz <s.a.fivaz@gmail.com>
2024-10-25 21:10:25 +0200

    remove svelte-headlessui from Toggle

--- src/lib/components/form/toggle/Toggle.svelte
+++ src/lib/components/form/toggle/Toggle.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Switch, SwitchGroup, SwitchLabel } from '@rgossiaux/svelte-headlessui';
 	import { clsx } from 'clsx';
 	import { createEventDispatcher } from 'svelte';
 
@@ -16,12 +15,11 @@
 	}
 </script>
 
-<SwitchGroup>
+<button on:click={() => (value = !value)}>
 	<input {name} type="hidden" value={value ? 'true' : ''} />
-	<div class="flex items-center gap-2">
-		<SwitchLabel class="flex-1 text-start">{label}</SwitchLabel>
-		<Switch
-			bind:checked={value}
+	<span class="flex items-center gap-2">
+		<span class="flex-1 text-start">{label}</span>
+		<span
 			class={clsx(
 				value ? 'bg-blue-600' : 'bg-gray-200',
 				'relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
@@ -33,6 +31,6 @@
 					'inline-block h-4 w-4 transform rounded-full bg-white transition-transform',
 				)}
 			/>
-		</Switch>
-	</div>
-</SwitchGroup>
+		</span>
+	</span>
+</button>
