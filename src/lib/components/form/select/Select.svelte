9dcd7f03671b1cf4bcb214c540ba433883a874ba
fivaz <s.a.fivaz@gmail.com>
2024-10-25 21:31:27 +0200

    remove svelte-headlessui from Select

--- src/lib/components/form/select/Select.svelte
+++ src/lib/components/form/select/Select.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-	import {
-		Listbox,
-		ListboxButton,
-		ListboxLabel,
-		ListboxOptions,
-	} from '@rgossiaux/svelte-headlessui';
 	import { ChevronUpDown } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { clsx } from 'clsx';
@@ -19,17 +13,20 @@
 	export let labelClass: string = '';
 
 	export let selectClass: string = '';
+
+	let isOpen: boolean = false;
 </script>
 
-<Listbox bind:value class={clsx(className, 'text-sm font-medium text-gray-700')}>
+<div class={clsx(className, 'text-sm font-medium text-gray-700')}>
 	{#if label}
-		<ListboxLabel class={labelClass}>
+		<div class={labelClass}>
 			{label}
-		</ListboxLabel>
+		</div>
 	{/if}
 	<div class={clsx(selectClass, 'relative')}>
-		<ListboxButton
+		<button
 			class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+			on:click={() => (isOpen = !isOpen)}
 		>
 			<span class="block truncate">
 				<slot name="placeholder" />
@@ -37,11 +34,11 @@
 			<span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
 				<Icon aria-hidden="true" class="h-5 w-5 text-gray-400" src={ChevronUpDown} />
 			</span>
-		</ListboxButton>
-		<ListboxOptions
+		</button>
+		<ul
 			class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
 		>
 			<slot />
-		</ListboxOptions>
+		</ul>
 	</div>
-</Listbox>
+</div>
--- src/lib/components/form/select/select-item/SelectItem.svelte
+++ src/lib/components/form/select/select-item/SelectItem.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { ListboxOption } from '@rgossiaux/svelte-headlessui';
 	import { Check } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { clsx } from 'clsx';
@@ -9,29 +8,22 @@
 
 	export let value: unknown;
 
-	let convertedValue: null | number | string | string[] | undefined = value as string;
+	let isSelected: boolean = false;
 </script>
 
-<ListboxOption let:active let:selected value={convertedValue}>
-	<li
-		class={clsx(
-			active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-			'relative cursor-default select-none py-2 pl-3 pr-9',
-		)}
-	>
-		<span class={clsx(active ? 'font-semibold' : 'font-normal', 'truncate', className)}>
-			<slot />
-		</span>
+<li
+	class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 hover:bg-indigo-600 hover:text-white"
+	data-value={value}
+>
+	<span class={clsx('truncate font-normal hover:font-semibold', className)}>
+		<slot />
+	</span>
 
-		{#if selected}
-			<span
-				class={clsx(
-					active ? 'text-white' : 'text-indigo-600',
-					'absolute inset-y-0 right-0 flex items-center pr-4',
-				)}
-			>
-				<Icon aria-hidden="true" class="h-5 w-5" src={Check} />
-			</span>
-		{/if}
-	</li>
-</ListboxOption>
+	{#if isSelected}
+		<span
+			class="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 hover:text-white"
+		>
+			<Icon aria-hidden="true" class="h-5 w-5" src={Check} />
+		</span>
+	{/if}
+</li>
