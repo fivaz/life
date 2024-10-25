cc78ff21c98db377bc60c450da67d8d8b6fcc7ee
fivaz <s.a.fivaz@gmail.com>
2024-10-25 21:24:21 +0200

    remove svelte-headlessui from Collapsable

--- src/lib/components/collapsable/Collapsable.svelte
+++ src/lib/components/collapsable/Collapsable.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-	import {
-		Disclosure,
-		DisclosureButton,
-		DisclosurePanel,
-		Transition,
-	} from '@rgossiaux/svelte-headlessui';
-	import { ChevronRight } from '@steeze-ui/heroicons';
+	
+
+import { ChevronRight } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 
 	export let title: string;
+
+	let isOpen: boolean = false;
 </script>
 
-<Disclosure class="rounded-lg bg-white p-2" let:open>
-	<DisclosureButton class="flex w-full justify-between">
+<div class="rounded-lg bg-white p-2">
+	<button class="flex w-full justify-between" on:click={() => (isOpen = !isOpen)}>
 		<span>{title}</span>
-		<Icon class="h-5 w-5 {open ? 'rotate-90 transform' : ''}" src={ChevronRight} />
-	</DisclosureButton>
-	<Transition
-		class="overflow-hidden transition-all duration-500"
-		enterFrom="transform opacity-0 max-h-0"
-		enterTo="transform opacity-100 max-h-36"
-		leaveFrom="transform opacity-100 max-h-36"
-		leaveTo="transform opacity-0 max-h-0"
-	>
-		<DisclosurePanel class="flex flex-col gap-2 pt-2 text-gray-500">
+		<Icon class="h-5 w-5 {isOpen ? 'rotate-90 transform' : ''}" src={ChevronRight} />
+	</button>
+	{#if isOpen}
+		<!--		<Transition-->
+		<!--			class="overflow-hidden transition-all duration-500"-->
+		<!--			enterFrom="transform opacity-0 max-h-0"-->
+		<!--			enterTo="transform opacity-100 max-h-36"-->
+		<!--			leaveFrom="transform opacity-100 max-h-36"-->
+		<!--			leaveTo="transform opacity-0 max-h-0"-->
+		<!--		>-->
+		<div class="flex flex-col gap-2 pt-2 text-gray-500">
 			<slot />
-		</DisclosurePanel>
-	</Transition>
-</Disclosure>
+		</div>
+		<!--		</Transition>-->
+	{/if}
+</div>
