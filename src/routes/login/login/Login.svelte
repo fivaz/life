bbf342060eb54e7c119818da80b1f7f5426ca9eb
fivaz <s.a.fivaz@gmail.com>
2024-11-01 23:07:52 +0100

    make auth pages pre rendered

--- src/routes/login/login/Login.svelte
+++ src/routes/login/login/Login.svelte
@@ -166,10 +166,6 @@
 	</div>
 
 	<div class="relative hidden w-0 flex-1 lg:block">
-		<img
-			alt=""
-			class="absolute inset-0 h-full w-full object-cover"
-			src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-		/>
+		<img alt="" class="absolute inset-0 h-full w-full object-cover" src="/login.avif" />
 	</div>
 </div>
