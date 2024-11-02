6e4f46f0222420514431701cd32af78ec305eace
fivaz <s.a.fivaz@gmail.com>
2024-11-02 02:19:00 +0100

    refactor

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,7 +3,7 @@ import { build, files, prerendered, version } from '$service-worker';
 import { precacheAndRoute } from 'workbox-precaching';
 
 const precache_list = [
-	'/', // Attention: serves stale index, might not be ideal for your use case.
+	'/',
 	'/demo',
 	'/login',
 	'/register',
