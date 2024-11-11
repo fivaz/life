4454f91fa913e346b0fa788bc11038aa6aed1764
fivaz <s.a.fivaz@gmail.com>
2024-11-11 02:44:17 +0100

    make pwa work only on production

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,19 @@
 
 	import { type Snippet } from 'svelte';
 
+	import { dev } from '$app/environment';
+
 	interface Props {
 		children: Snippet;
 	}
 
 	let { children }: Props = $props();
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<link rel="manifest" href="/manifest.json" />
+	{/if}
+</svelte:head>
 
 {@render children()}
