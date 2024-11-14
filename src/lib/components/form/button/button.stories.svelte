4be820eda3b22ecb362b09759277fca505e064c1
fivaz <s.a.fivaz@gmail.com>
2024-11-14 01:31:08 +0100

    create a special type to add new props to a story

--- src/lib/components/form/button/button.stories.svelte
+++ src/lib/components/form/button/button.stories.svelte
@@ -1,49 +1,55 @@
 <script module lang="ts">
 	import { Trash } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { type Args, defineMeta, setTemplate } from '@storybook/addon-svelte-csf';
 
+	import type { IncreaseArgs } from '../../../../../.storybook/special-types';
 	import Button from './Button.svelte';
 
-	const { Story } = defineMeta({
+	const { Story } = defineMeta<IncreaseArgs<typeof Button, { isOnlyIcon: boolean }>>({
 		component: Button,
+		argTypes: {
+			isOnlyIcon: {
+				control: { type: 'boolean' },
+			},
+		},
+		args: {
+			isOnlyIcon: true,
+			onclick: () => console.log('clicked1'),
+		},
 	});
 </script>
 
-<Story name="Primary">
-	<Button color="indigo" disabled={false} isLoading={false} onclick={() => console.log('clicked')}>
-		<Icon class="h-4 w-4" src={Trash} />Button
-	</Button>
-</Story>
+<script lang="ts">
+	setTemplate(template);
+</script>
 
-<!--{#snippet template(args: any)}-->
-<!--	{#if !args.isOnlyIcon}-->
-<!--		<Button {...args} }><Icon class="h-4 w-4" src={Trash} />Button</Button>-->
-<!--	{:else}-->
-<!--		<Button {...args} }><Icon class="h-4 w-4" src={Trash} /></Button>-->
-<!--	{/if}-->
-<!--{/snippet}-->
+{#snippet template(args: Args<typeof Story>)}
+	{#if args.isOnlyIcon}
+		<Button {...args}><Icon class="h-4 w-4" src={Trash} /></Button>
+	{:else}
+		<Button {...args}><Icon class="h-4 w-4" src={Trash} />Button</Button>
+	{/if}
+{/snippet}
 
-<!--<Story-->
-<!--	args={{-->
-<!--		color: 'indigo',-->
-<!--		disabled: false,-->
-<!--		isLoading: false,-->
-<!--		onclick: () => console.log('clicked'),-->
-<!--		isOnlyIcon: false,-->
-<!--	}}-->
-<!--	name="Primary"-->
-<!--	children={template}-->
-<!--/>-->
+<Story
+	args={{
+		color: 'indigo',
+		disabled: false,
+		isLoading: false,
+		isOnlyIcon: false,
+	}}
+	name="Primary"
+	children={template}
+/>
 
-<!--<Story-->
-<!--	args={{-->
-<!--		color: 'indigo',-->
-<!--		disabled: false,-->
-<!--		isLoading: false,-->
-<!--		onclick: () => console.log('clicked'),-->
-<!--		isOnlyIcon: true,-->
-<!--	}}-->
-<!--	name="With only Icon"-->
-<!--	children={template}-->
-<!--/>-->
+<Story
+	args={{
+		color: 'indigo',
+		disabled: false,
+		isLoading: false,
+		isOnlyIcon: true,
+	}}
+	name="With only Icon"
+	children={template}
+/>
