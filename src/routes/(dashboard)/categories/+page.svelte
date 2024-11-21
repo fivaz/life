91830dc72907e5891a6ee83ca19d54cbd1f9abdd
fivaz <s.a.fivaz@gmail.com>
2024-11-21 22:53:29 +0100

    fetch firestore collections in the setup rather than in components in categories page

--- src/routes/(dashboard)/categories/+page.svelte
+++ src/routes/(dashboard)/categories/+page.svelte
@@ -3,7 +3,7 @@
 
 	import type { Category } from '$lib/category/category.model';
 	import { buildEmptyCategory } from '$lib/category/category.model';
-	import DBCategories from '$lib/category/DBCategories.svelte';
+	import { fetchCategories } from '$lib/category/category.respository';
 	import Button from '$lib/components/form/button/Button.svelte';
 	import Modal from '$lib/components/modal/Modal.svelte';
 	import { title } from '$lib/date.utils.svelte';
@@ -21,29 +21,29 @@
 		isFormOpen = true;
 		editingCategory = category;
 	}
+
+	let categories = $state<Category[]>([]);
+
+	fetchCategories(categories);
 </script>
 
-<DBCategories>
-	{#snippet data(categories)}
-		<div class="mx-auto flex max-w-7xl flex-col gap-5 p-4 sm:px-6 lg:px-8">
-			<div class="flex items-center justify-between">
-				<h1 class="hidden text-2xl font-bold text-gray-900 md:block">{title.value}</h1>
-				<span></span>
-				<Button onclick={() => openForm()}>
-					<Plus class="h-4 w-auto" />
-					New Category
-				</Button>
-			</div>
+<div class="mx-auto flex max-w-7xl flex-col gap-5 p-4 sm:px-6 lg:px-8">
+	<div class="flex items-center justify-between">
+		<h1 class="hidden text-2xl font-bold text-gray-900 md:block">{title.value}</h1>
+		<span></span>
+		<Button onclick={() => openForm()}>
+			<Plus class="h-4 w-auto" />
+			New Category
+		</Button>
+	</div>
 
-			<ul class="flex flex-col gap-1">
-				{#each categories as category (category.id)}
-					<CategoryRow {category} edit={openForm} />
-				{/each}
-			</ul>
+	<ul class="flex flex-col gap-1">
+		{#each categories as category (category.id)}
+			<CategoryRow {category} edit={openForm} />
+		{/each}
+	</ul>
 
-			<Modal bind:isOpen={isFormOpen}>
-				<CategoryForm category={editingCategory} close={() => (isFormOpen = false)} />
-			</Modal>
-		</div>
-	{/snippet}
-</DBCategories>
+	<Modal bind:isOpen={isFormOpen}>
+		<CategoryForm category={editingCategory} close={() => (isFormOpen = false)} />
+	</Modal>
+</div>
