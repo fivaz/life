f90b589f78704c8757320c7e862a33969f1cb913
fivaz <s.a.fivaz@gmail.com>
2025-03-16 23:54:18 +0100

    refactor

--- packages/shared/src/lib/components/week-changer/WeekChanger.svelte
+++ packages/shared/src/lib/components/week-changer/WeekChanger.svelte
@@ -4,14 +4,17 @@
 	import ChevronLeftIcon from 'lucide-svelte/icons/chevron-left';
 	import ChevronRightIcon from 'lucide-svelte/icons/chevron-right';
 
+	import { borderColors } from '$lib/colors.js';
+	import Button from '$lib/components/button/Button.svelte';
 	import { DATE, formatDate } from '$lib/date.js';
 
 	interface Props {
 		selectedDate: Date;
 		onchange?: (weekStart: Date) => void;
+		class?: string;
 	}
 
-	let { onchange, selectedDate = $bindable() }: Props = $props();
+	let { onchange, selectedDate = $bindable(), class: klass }: Props = $props();
 
 	const currentDate = new Date();
 
@@ -46,37 +49,36 @@
 	}
 </script>
 
-<div class="flex items-center">
-	<div class="relative flex items-stretch rounded-md shadow-sm">
-		<button
-			class="hmd:h-9 -8 flex w-9 items-center justify-center rounded-l-md border-y border-l border-gray-300 pr-0 text-gray-400 hover:bg-gray-200 hover:text-gray-500 focus:relative dark:border-gray-700 dark:hover:bg-gray-800"
-			onclick={goToPreviousWeek}
-			type="button"
-		>
-			<ChevronLeftIcon class="size-6" />
-		</button>
+<div class="relative flex items-center {klass} rounded-md border {borderColors.darker}">
+	<button
+		class="px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-700"
+		onclick={goToPreviousWeek}
+		type="button"
+	>
+		<ChevronLeftIcon class="size-6" />
+	</button>
 
-		<!--this input is triggered by the button below-->
-		<input
-			bind:this={datePicker}
-			class="pointer-events-none absolute touch-none opacity-0 dark:[color-scheme:dark]"
-			type="date"
-			bind:value={selectedDateString.value}
-		/>
+	<!--this input is triggered by the button below-->
+	<input
+		bind:this={datePicker}
+		class="pointer-events-none absolute touch-none opacity-0 dark:[color-scheme:dark]"
+		type="date"
+		bind:value={selectedDateString.value}
+	/>
 
-		<button
-			class="border-y border-gray-300 px-3.5 text-sm font-semibold hover:bg-gray-200 focus:relative dark:border-gray-700 dark:hover:bg-gray-800"
-			onclick={openDatePicker}
-		>
-			<CalendarCheck class="size-5 text-gray-400" />
-		</button>
+	<button
+		class="px-2 py-1.5 hover:bg-gray-200 dark:hover:bg-gray-700"
+		onclick={openDatePicker}
+		type="button"
+	>
+		<CalendarCheck class="size-5" />
+	</button>
 
-		<button
-			class="flex h-8 w-9 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-0 text-gray-400 hover:bg-gray-200 hover:text-gray-500 focus:relative md:h-9 dark:border-gray-700 dark:hover:bg-gray-800"
-			onclick={goToNextWeek}
-			type="button"
-		>
-			<ChevronRightIcon class="size-6" />
-		</button>
-	</div>
+	<button
+		class="px-2 py-1 hover:bg-gray-200 dark:hover:bg-gray-700"
+		onclick={goToNextWeek}
+		type="button"
+	>
+		<ChevronRightIcon class="size-6" />
+	</button>
 </div>
