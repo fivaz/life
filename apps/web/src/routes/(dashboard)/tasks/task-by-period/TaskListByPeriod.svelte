db0d14df38390ad663b53f7f2d9a9f0645b13bf4
fivaz <s.a.fivaz@gmail.com>
2025-04-27 01:11:31 +0200

    style

--- apps/web/src/routes/(dashboard)/tasks/task-by-period/TaskListByPeriod.svelte
+++ apps/web/src/routes/(dashboard)/tasks/task-by-period/TaskListByPeriod.svelte
@@ -38,31 +38,29 @@
 	class="flex flex-col gap-2 divide-gray-300 rounded-lg border border-gray-300 bg-gray-50 p-3 dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900"
 >
 	<TaskListHeaderByPeriod {listName} {tasksByPeriod} />
-	{#if tasksByPeriod[listName].length}
-		<ul
-			class="flex flex-col gap-2"
-			onconsider={handleDndConsider}
-			onfinalize={handleDndFinalize}
-			use:dndzone={{
-				items: tasksByPeriod[listName],
-				flipDurationMs,
-				dragDisabled: listName.startsWith('recurring'),
-				dropFromOthersDisabled: !isDroppable,
-			}}
-		>
-			{#each tasksByPeriod[listName] as task (task.id)}
-				<li animate:flip={{ duration: flipDurationMs }}>
-					<TaskByPeriod {task} />
-				</li>
-			{/each}
-		</ul>
-	{/if}
-
-	{#if isDroppable}
-		<LText
-			class="relative flex h-10 select-none items-center justify-center gap-2 divide-gray-300 rounded-lg border-2 border-dashed border-gray-300 p-1 hover:border-gray-500 dark:divide-gray-700 dark:border-gray-700"
-		>
-			<ClipboardCopyIcon class="size-5" /> drop a task here
-		</LText>
-	{/if}
+	<ul
+		class="flex flex-col gap-2"
+		onconsider={handleDndConsider}
+		onfinalize={handleDndFinalize}
+		use:dndzone={{
+			items: tasksByPeriod[listName],
+			flipDurationMs,
+			dragDisabled: listName.startsWith('recurring'),
+			dropFromOthersDisabled: !isDroppable,
+			dropTargetStyle: {},
+		}}
+	>
+		{#each tasksByPeriod[listName] as task (task.id)}
+			<li animate:flip={{ duration: flipDurationMs }}>
+				<TaskByPeriod {task} />
+			</li>
+		{/each}
+		{#if isDroppable}
+			<LText
+				class="relative flex h-10 select-none items-center justify-center gap-2 divide-gray-300 rounded-lg border-2 border-dashed border-gray-300 p-1 hover:border-gray-500 dark:divide-gray-700 dark:border-gray-700"
+			>
+				<ClipboardCopyIcon class="size-5" /> drop a task here
+			</LText>
+		{/if}
+	</ul>
 </section>
