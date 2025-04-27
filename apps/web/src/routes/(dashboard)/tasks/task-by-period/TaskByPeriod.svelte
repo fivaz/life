cb69db1b399e1ef6df9f7c175b00891ff6e17a8c
fivaz <s.a.fivaz@gmail.com>
2025-04-27 11:26:08 +0200

    refactor

--- apps/web/src/routes/(dashboard)/tasks/task-by-period/TaskByPeriod.svelte
+++ apps/web/src/routes/(dashboard)/tasks/task-by-period/TaskByPeriod.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { tailwindColorMap } from '@life/shared/category';
 	import type { Task } from '@life/shared/task';
+	import { clsx } from 'clsx';
 	import { format } from 'date-fns';
-	import { Settings2Icon } from 'lucide-svelte';
+	import { GripVerticalIcon, Settings2Icon } from 'lucide-svelte';
+	import { dragHandle } from 'svelte-dnd-action';
 
 	import TaskFormButton from '$lib/task/task-form/TaskFormButton.svelte';
 	import { DATE_FR, DATE_FR_SHORT } from '$lib/utils.svelte';
@@ -24,13 +26,16 @@
 
 <div
 	class="{tailwindColorMap[task.category.color].darkBg}
-		flex select-none justify-between gap-3 rounded-lg px-3 py-2 text-sm font-semibold leading-6 text-gray-50 sm:touch-none"
+		flex select-none items-center justify-between gap-3 rounded-lg px-3 py-2 text-sm font-semibold leading-6 text-gray-50 sm:touch-none"
 >
 	<div class="flex gap-3">
 		<div class="flex items-center justify-between gap-3 sm:hidden">
 			<!--to avoid dragging during scroll on touch devices, it's only possible to drag a task if it's grabbed by the handle-->
-			<div class="w-10">{formatTaskDate(task, DATE_FR_SHORT)}</div>
-			<div class="name w-[calc(100%-64px)]">{task.name}</div>
+			<div aria-label="drag-handle for {task.id}" use:dragHandle>
+				<GripVerticalIcon class="size-5" />
+			</div>
+			<div class={clsx({ 'w-10': task.date })}>{formatTaskDate(task, DATE_FR_SHORT)}</div>
+			<div class={clsx('name', { 'w-[calc(100%-64px)]': task.date })}>{task.name}</div>
 		</div>
 		<div class="hidden gap-3 sm:flex">
 			{formatTaskDate(task, DATE_FR)}
@@ -54,7 +59,7 @@
 </div>
 
 <style>
-	/* make the test truncate in two lines */
+	/* make the text truncate in two lines */
 	.name {
 		display: -webkit-box;
 		line-clamp: 2;
