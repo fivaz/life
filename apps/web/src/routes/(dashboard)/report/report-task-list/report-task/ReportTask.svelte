fce7aaab56eef4c83df06c324ecc252c34bab7da
fivaz <31249205+fivaz@users.noreply.github.com>
2025-08-01 17:36:26 +0200

    mark when old tasks are completed

--- apps/web/src/routes/(dashboard)/report/report-task-list/report-task/ReportTask.svelte
+++ apps/web/src/routes/(dashboard)/report/report-task-list/report-task/ReportTask.svelte
@@ -1,6 +1,8 @@
-<script lang="ts">
+<script lang='ts'>
+	import { parseDate } from '@life/shared/date';
 	import type { Task } from '@life/shared/task';
-	import { CalendarMinus, CalendarPlus, Sparkles } from 'lucide-svelte';
+	import { isBefore, startOfDay } from 'date-fns';
+	import { ArchiveIcon, ArchiveXIcon, CalendarMinus, CalendarPlus } from 'lucide-svelte';
 
 	import { tooltip } from '$lib/components/tooltip/tooltip.action';
 
@@ -11,6 +13,15 @@
 	}
 
 	let { task, index, isAdd = false }: Props = $props();
+
+	function isOld(task: Task): boolean {
+		try {
+			return isBefore(startOfDay(new Date(task.createdAt)), startOfDay(parseDate(task.date)));
+		} catch (e) {
+			console.log(e);
+			return false;
+		}
+	}
 </script>
 
 <li
@@ -18,20 +29,24 @@
 	{isAdd ? 'bg-red-500 dark:bg-red-600' : 'bg-green-500 dark:bg-green-600'}
 	flex justify-between gap-x-3 rounded-lg px-3 py-2 text-sm font-semibold leading-6 text-white"
 >
-	<div class="flex items-center gap-x-2">
+	<div class='flex items-center gap-x-2'>
 		{index}
 		{#if isAdd}
-			<CalendarPlus class="size-5 text-white" />
+			<CalendarPlus class='size-5 text-white' />
 		{:else}
-			<CalendarMinus class="size-5 text-white" />
+			<CalendarMinus class='size-5 text-white' />
 		{/if}
-		<div class="block truncate text-sm font-semibold leading-6">{task.name}</div>
+		<div class='block truncate text-sm font-semibold leading-6'>{task.name}</div>
 	</div>
 
 	<div>
 		{#if isAdd && !task.isDone}
 			<div use:tooltip={'task remains undone'}>
-				<Sparkles class="size-5 text-white" />
+				<ArchiveIcon class='size-5 text-white' />
+			</div>
+		{:else if !isAdd && task.isDone && isOld(task)}
+			<div use:tooltip={'old task completed'}>
+				<ArchiveXIcon class='size-5 text-white' />
 			</div>
 		{/if}
 	</div>
