73c5f79a7449402e6c77a9fc30b49e0ab651c897
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-14 15:09:31 +0100

    update web to tailwind 4

--- apps/web/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'../../packages/shared/src/**/*.{html,js,ts,svelte}',
-		'../../packages/svelendar/src/**/*.{html,js,ts,svelte}',
-	],
-} satisfies Config;
