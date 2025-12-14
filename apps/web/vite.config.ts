7cc6c2c17f018b7b24551af4d3d75ce69e890601
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-14 22:01:27 +0100

    update some config files

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,7 +2,9 @@ import 'dotenv/config';
 
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
 
 // Detect production mode
 // eslint-disable-next-line turbo/no-undeclared-env-vars
@@ -14,6 +16,7 @@ export default defineConfig({
 		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
 	},
 	plugins: [
+		tailwindcss(),
 		// Only include Sentry in production
 		...(isProduction
 			? [
@@ -27,4 +30,35 @@ export default defineConfig({
 			: []),
 		sveltekit(),
 	],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+
+				test: {
+					name: 'client',
+
+					browser: {
+						enabled: true,
+						provider: playwright(),
+						instances: [{ browser: 'chromium', headless: true }],
+					},
+
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+				},
+			},
+			{
+				extends: './vite.config.ts',
+
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+				},
+			},
+		],
+	},
 });
