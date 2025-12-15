a8237894b789f6084aa325180474fb44d3f99a23
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-15 10:21:32 +0100

    wip eslint

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,3 +1,71 @@
-import sharedConfig from '@life/config-eslint';
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+// import storybook from 'eslint-plugin-storybook';
 
-export default [...sharedConfig];
+import prettier from 'eslint-config-prettier';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+import turboConfig from 'eslint-config-turbo/flat';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default defineConfig(
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
+	...turboConfig,
+	prettier,
+	...svelte.configs.prettier,
+	{
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off',
+		},
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig,
+			},
+		},
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+			// 'unused-imports/no-unused-imports': 'error',
+		},
+	},
+	{
+		rules: {
+			'svelte/no-navigation-without-resolve': [
+				'error',
+				{
+					ignoreGoto: false,
+					ignoreLinks: true,
+					ignorePushState: false,
+					ignoreReplaceState: false,
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.stories.svelte'],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+		},
+	},
+);
