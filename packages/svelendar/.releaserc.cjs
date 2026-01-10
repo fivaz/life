50c9b6eef8f3960c4b2b1d3f988720cde89b3352
fivaz <31249205+fivaz@users.noreply.github.com>
2026-01-10 15:15:06 +0100

    build(svelendar): make act skip npm publishing

---
+++ packages/svelendar/.releaserc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+	extends: 'semantic-release-monorepo',
+	branches: ['main'],
+	tagFormat: 'svelendar-v${version}',
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				preset: 'conventionalcommits',
+				presetConfig: {
+					types: [
+						{ type: 'feat', section: '✨ New Features', hidden: false },
+						{ type: 'fix', section: '🐛 Bug Fixes', hidden: false },
+						{ type: 'perf', section: '🚀 Performance', hidden: false },
+						{ type: 'revert', section: '⏪ Reverts', hidden: false },
+						{ type: 'chore', hidden: true },
+						{ type: 'docs', hidden: true },
+						{ type: 'style', hidden: true },
+						{ type: 'refactor', hidden: true },
+						{ type: 'test', hidden: true },
+						{ type: 'build', hidden: true },
+						{ type: 'ci', hidden: true },
+					],
+				},
+			},
+		],
+		['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+		['@semantic-release/npm', { npmPublish: !process.env.DRY_RUN }],
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json', 'CHANGELOG.md'],
+				message:
+					'chore(svelendar): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		'@semantic-release/github',
+	],
+};
